392cbd8a3089e7ca4799b23a1da1bb38ce828ed6
sonnymilton <sonny@milton.pro>
2024-05-07 01:17:37 +0700

    Add supervisor for  consumer

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 composer dump-env prod
-bin/console cache:clear
+bin/console cache:clear && chown www-data:www-data var/cache -R
 bin/console doctrine:database:create --if-not-exists -n
 bin/console doctrine:migrations:migrate -n --allow-no-migration
+supervisord -c /etc/supervisor/supervisord.conf && supervisorctl reread && supervisorctl update && supervisorctl start messenger-consume:*
 php-fpm
 nginx
-bin/console messenger:consume async --time-limit=3600
 bash
