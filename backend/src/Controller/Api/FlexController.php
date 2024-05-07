b0f0f451e47799d1bf00211937dbd0a8674d4039
sonnymilton <sonny@milton.pro>
2024-05-08 04:23:23 +0700

    Add openapi docs

--- backend/src/Controller/Api/FlexController.php
+++ backend/src/Controller/Api/FlexController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controller\Api;
 
 use App\Flex\Index\IndexLoaderInterface;
+use OpenApi\Attributes as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,6 +14,7 @@ use Symfony\Component\Routing\Annotation\Route;
  * @phpstan-import-type FlexIndex from IndexLoaderInterface
  */
 #[Route('/flex')]
+#[OA\Tag('Flex')]
 final class FlexController extends AbstractController
 {
     /** @return FlexIndex */
